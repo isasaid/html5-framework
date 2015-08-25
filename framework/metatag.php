5ebccdcafa1378d774cc307f49b3e68a66851858
isamsaid <yokeofphoebe@outlook.com>
2015-08-25 10:17:54 +0700

    html framework
    framework

---
+++ framework/metatag.php
@@ -0,0 +1,82 @@
+﻿	﻿<?php include '../config/config.php' //config ?>
+    
+	<!--BASICS META TAGS-->	
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="Description" content="<?php include $app_description //description ?>">
+    <meta name="Keywords" content="<?php include $app_keywords //keywords ?>">
+	<meta name="robots" content="index,follow">
+	<meta name="alexaVerifyID" content="<?php echo $alexaid //alexaid ?>"/>
+	<meta name="google-site-verification" content="<?php echo $googleid //googleid ?>" />
+    <meta http-equiv="Cache-control" content="public">
+
+	<META NAME="author" content="<?php echo $author //author ?>">
+	<META NAME="copyright" content="<?php echo $copyright //copyright ?>">
+	<META NAME="language" content="<?php echo $language //language ?>">
+	<META NAME="rating" content="<?php echo $rating //rating ?>">
+	<META NAME="revisit-after" content="1 Days">
+	<META http-equiv="pragma" content="no-cache">	
+	<meta content='<?php echo $email //email ?>' name='email'/>
+	<meta content='Global' name='Distribution'/>
+	
+	
+	
+
+	<!--MOBILE META TAGS-->	
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	
+		<!--SOCIAL META TAGS-->	
+	
+	<!-- Twitter Card data -->
+	<meta name="twitter:card" content="product">
+	<meta name="twitter:description" content="<?php include $app_descriptiontwitter //description ?>" >
+	<meta name="twitter:site" content="<?php echo $twitter //twitter ?>">
+	<meta name="twitter:creator" content="<?php echo $twitter //twitter ?>">
+	<meta name="twitter:domain" content="<?php echo $domain //domain ?>">
+	<meta name="twitter:title" content="<?php include $app_titletwitter //title ?>">
+	<meta name="twitter:image" content="<?php echo $image //image ?>">
+	<meta name="twitter:label1" content="Genre">
+	<meta name="twitter:data1" content="HTML5">
+	<meta name="twitter:label2" content="Location">
+	<meta name="twitter:data2" content="National">
+	
+	<!-- Open Graph data -->
+	<meta property="og:title" content="<?php include $app_titleog //title ?>" />
+	<meta property="og:type" content="product" />
+	<meta property="og:url" content="<?php echo $url //url ?>" />
+	<meta property="og:image" content="<?php echo $image //image ?>" />
+	<meta property="og:site_name" content="<?php include $app_titleog //title ?>" />
+	<meta property="og:description" content="<?php include $app_descriptionog //description ?>" >
+	<meta property="og:price:amount" content="0.00" />
+	<meta property="og:price:currency" content="USD" />
+	<meta property="og:type" content="product" />
+	<meta property="og:price:standard_amount" content="10.00" />
+	<meta property="og:price:currency" content="USD" />
+	<meta property="og:availability" content="instock" />
+	<meta property="og:see_also" content="goo.gl/Nnwemo" />
+	<meta property="og:rating" content="4.5" />
+	<meta property="og:rating_scale" content="5" />
+	<meta property="og:rating_count" content="120" />
+	<meta property="product:gender" content="male" >
+	<meta property="product:color" content="Aqua" >
+	<meta property="product:color:map" content="blue" >
+	<meta property="product:color:image" content="<?php echo $image //image ?>" >
+	
+	<!--MICRODATA META TAGS-->	
+	<meta itemprop="name" content="<?php include $app_titlemicrodata //title ?>">
+	<meta itemprop="description" content="<?php include $app_descriptionmicrodata //description ?>" >
+	<meta itemprop="image" content="<?php echo $image //image ?>" />
+	<meta itemprop="bookFormat" content="EBook/Docx"/>
+    <meta itemprop="accessibilityFeature" content="longDescription"/>
+    <meta itemprop="accessibilityFeature" content="alternativeText"/>
+    <meta itemprop="accessibilityControl" content="fullMouseControl"/>
+    <meta itemprop="accessibilityHazard" content="noFlashing"/>
+    <meta itemprop="accessibilityHazard" content="noMotionSimulation"/>
+    <meta itemprop="accessibilityHazard" content="noSound"/>
+	<meta itemprop="url" content="goo.gl/Nnwemo" />
+	<meta itemprop="image" content="<?php echo $image //image ?>" />
+	<meta itemprop="relatedItem" content="<?php echo $relateditem //relateditem ?>" />
+	<meta itemprop="offers" content="http://schema.org/Offer">
+	<meta itemprop="availability" content="http://schema.org/InStock" />
+			
