5a6a95a5e91172f2953edb0085fe94f3991733a6
isamsaid <isa.muhammad.said@gmail.com>
2015-08-25 10:29:13 +0700

    html5 framework

---
+++ framework/link-rel.php
@@ -0,0 +1,49 @@
+﻿	﻿	<?php include '../config/config.php' //config ?>
+	
+	<!--HUMANS TXT-->	
+	<link rel="author" href="<?php echo $websiteurl //websiteurl ?>" />
+
+	<!--ROBOTS TXT-->	
+	<link rel="tag" href="<?php echo $websiteurl //websiteurl ?>robots.txt" />
+    
+	<!--SITEMAP XML-->	
+	<link rel="tag" href="<?php echo $websiteurl //websiteurl ?>" />
+	
+	<!--DNS LOADER HTML-->	
+	<link rel="tag" href="<?php echo $websiteurl //websiteurl ?>" />
+	
+	<!--CANONICAL LINKS-->
+	<link rel="canonical" href="<?php echo $websiteurl //websiteurl ?>" />
+	
+	<!--CONTENTS LINKS-->
+	<link rel="contents" href="<?php echo $websiteurl //websiteurl ?>" />
+    
+    
+	
+	
+<link rel="stylesheet" type="text/css" href="<?php echo $app_css //app_css ?>" media="screen" />
+
+<link rel="canonical" href="<?php echo $canonicalurl //canonicalurl ?>" />
+<link rel="prefetch" href="<?php echo $prefetchurl //prefetchurl ?>" />
+	
+	<!-- ==============================================
+	Google Fonts
+	=============================================== -->	
+	<link href='' rel='stylesheet' type='text/css'>
+	
+	
+	
+	<link rel="pingback" href="<?php echo $websiteurl //websiteurl ?>">
+	
+    <link rel="tag" href="<?php echo $websiteurl //websiteurl ?>">	
+	
+	<link rel="shortcut icon" href="<?php echo $websiteurl //websiteurl ?>favicon.ico" type="image/x-icon">
+	
+	
+	<link rel="author" href="<?php echo $googleauthor //googleauthor ?>" />
+	
+	<link rel="publisher" href="<?php echo $googlepublisher //googlepublisher ?>" />
+    
+<!--[if IE]>
+<link rel="stylesheet" type="text/css" href="<?php echo $app_css2 //app_css2 ?>" media="screen" />
+<![endif]-->
---
+++ framework/sidebar.php
@@ -0,0 +1,10 @@
+<div id="sidebar">
+
+<?php include $app_sidebar_ads //sidebar-ads ?>
+
+<?php include $app_sidebar_about //sidebar-about ?>
+
+<?php include $app_sidebar_messages //keywords ?>
+
+
+</div>
