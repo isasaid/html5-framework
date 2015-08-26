dd0822e00b115f3ff2b6b54bd7974793cb1c9172
isamsaid <isa.muhammad.said@gmail.com>
2015-08-26 10:09:34 +0700

    html framework
    html framework

---
+++ config/README.txt
@@ -0,0 +1,2 @@
+rename contig.php to config.php.bak
+rename config-landingpage-example.php to config.php
---
+++ config/config-landingpage-example.php
@@ -0,0 +1,64 @@
+<?php
+
+# Website Project (a or b or c website project)
+$app_title = '../title/c.txt';
+$app_content = '../content/landingpage.html';
+$app_description = '../description/c.txt';
+$app_keywords = '../keywords/c.txt';
+$app_descriptiontwitter = '../description/c-twitter.txt';
+$app_titletwitter = '../title/c-twitter.txt';
+$app_titleog = '../title/c-og.txt';
+$app_descriptionog = '../description/c-og.txt';
+$app_titlemicrodata = '../title/c-microdata.txt';
+$app_descriptionmicrodata = '../description/c-microdata.txt';
+
+	# Sidebar
+$app_sidebar_ads = '../sidebar/nosidebar.txt';
+$app_sidebar_about = '../sidebar/nosidebar.txt';
+$app_sidebar_messages = '../sidebar/nosidebar.txt';
+
+	# External source
+#$app_css = 'https://googlnnwemo.appspot.com/googlnnwemo.appspot.com/stylesheets/style.css';
+#$app_css2 = 'https://googlnnwemo.appspot.com/googlnnwemo.appspot.com/stylesheets/ie.css';
+$app_css = '../stylesheets/common.css';
+
+	# Theme
+$theme_style = '../theme/landingpage/style.html';
+$theme_menu = '../theme/landingpage/menu.php';
+$theme_footer = '../theme/landingpage/footer.html';
+
+	# Website Menu
+$firstmenu = '';
+$menu1 = '';
+$menu2 = '';
+$menu3 = '';
+$menu4 = '';
+$menu5 = '';
+
+	# Header
+
+$header_title = '';
+$header_subtitle = '';
+
+# Website & Owner Identity
+
+$author = 'isasaid';
+$googleauthor = 'https://plus.google.com/u/0//posts';
+$googlepublisher = 'https://plus.google.com/u/0/';
+$copyright = 'html5-framework';
+$language = 'en-us';
+$rating = 'General';
+$email = 'owner@html5-framework.appspot.com';
+$twitter = '@yokeofphoebe';
+$domain = 'html5-framework.appspot.com';
+$alexaid = '';
+$googleid = '';
+$url = 'https://html5-framework.appspot.com/';
+$websiteurl = 'https://html5-framework.appspot.com/';
+$websiteurlproxy = 'https://googlnnwemo.appspot.com/html5-framework.appspot.com/';
+$canonicalurl = 'https://googlnnwemo.appspot.com/';
+$prefetchurl = 'https://googlnnwemo.appspot.com/';
+
+$relateditem = 'https://googlnnwemo.appspot.com/html5-framework.appspot.com/';
+
+$image = 'https://googlnnwemo.appspot.com/html5-framework.appspot.com/images/html5-badge-h-css3-performance-semantics.png';
