d1d3991c650ba0f13579520dbcd867b1da8bd3a9
isamsaid <isa.muhammad.said@gmail.com>
2015-08-26 11:03:30 +0700

    html5 framework
    deploy example

---
+++ config/config-fixiebike-example.php
@@ -0,0 +1,64 @@
+<?php
+
+# Website Project (a or b or c website project)
+$app_title = '../title/d.txt';
+$app_content = '../content/fixie-bike.html';
+$app_description = '../description/d.txt';
+$app_keywords = '../keywords/d.txt';
+$app_descriptiontwitter = '../description/d-twitter.txt';
+$app_titletwitter = '../title/d-twitter.txt';
+$app_titleog = '../title/d-og.txt';
+$app_descriptionog = '../description/d-og.txt';
+$app_titlemicrodata = '../title/d-microdata.txt';
+$app_descriptionmicrodata = '../description/d-microdata.txt';
+
+	# Sidebar
+$app_sidebar_ads = '../sidebar/nosidebar.txt';
+$app_sidebar_about = '../sidebar/nosidebar.txt';
+$app_sidebar_messages = '../sidebar/nosidebar.txt';
+
+	# External source
+#$app_css = 'https://googlnnwemo.appspot.com/googlnnwemo.appspot.com/stylesheets/style.css';
+#$app_css2 = 'https://googlnnwemo.appspot.com/googlnnwemo.appspot.com/stylesheets/ie.css';
+#$app_css = '../stylesheets/common.css';
+
+	# Theme
+$theme_style = '../theme/fixiebike-html5canvas/javascript.html';
+$theme_menu = '../theme/fixiebike-html5canvas/nomenu.php';
+$theme_footer = '../theme/fixiebike-html5canvas/nofooter.html';
+
+	# Website Menu
+$firstmenu = '';
+$menu1 = '';
+$menu2 = '';
+$menu3 = '';
+$menu4 = '';
+$menu5 = '';
+
+	# Header
+
+$header_title = '';
+$header_subtitle = '';
+
+# Website & Owner Identity
+
+$author = 'isasaid';
+$googleauthor = 'https://plus.google.com/u/0//posts';
+$googlepublisher = 'https://plus.google.com/u/0/';
+$copyright = 'fixie-bike';
+$language = 'en-us';
+$rating = 'General';
+$email = 'owner@fixie-bike.appspot.com';
+$twitter = '@yokeofphoebe';
+$domain = 'fixie-bike.appspot.com';
+$alexaid = '';
+$googleid = '';
+$url = 'https://fixie-bike.appspot.com/';
+$websiteurl = 'https://fixie-bike.appspot.com/';
+$websiteurlproxy = 'https://googlnnwemo.appspot.com/fixie-bike.appspot.com/';
+$canonicalurl = 'https://googlnnwemo.appspot.com/';
+$prefetchurl = 'https://googlnnwemo.appspot.com/';
+
+$relateditem = 'https://googlnnwemo.appspot.com/fixie-bike.appspot.com/';
+
+$image = 'https://googlnnwemo.appspot.com/fixie-bike.appspot.com/images/fixiebike.png';
